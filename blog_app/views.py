2423b3f1dc9220dd78059f8bc8bd6e9f678e0f49
Frank Miguel Rondon Rodriguez <fmrondon14@gmail.com>
2023-12-02 11:30:48 -0400

    homepage and about page

---
+++ blog_app/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+
+
+class HomepageView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
