f695a0290a0bcdfddecf0d3a447524c0949a1a1a
Frank Miguel Rondon Rodriguez <fmrondon14@gmail.com>
2024-01-20 11:37:37 -0400

    create new post

--- blog_app/url.py
+++ blog_app/url.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomepageView, AboutPageView
+from .views import HomepageView, AboutPageView, BlogDetailView, BlogCreateView
 
 urlpatterns = [
     path("", HomepageView.as_view(), name="home"),
-    path("about/", AboutPageView.as_view(), name="about")
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name='post_detail'),
+    path("post/new", BlogCreateView.as_view(), name="post_new")
 ]
